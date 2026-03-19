e86f71e3726d482db908a497247b97d55668688d
edwardeckelberry <edward.eckelberry@gmail.com>
2026-03-18 20:58:24 -0700

    init

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 import * as THREE from 'three';
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-
 renderer.setAnimationLoop( animate );
-
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
